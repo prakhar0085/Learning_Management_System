8a0e91977b296c48f5ca30b7c4907a3802e5bf8b
prakhar <132892214+prakhar0085@users.noreply.github.com>
2025-12-20 20:22:38 +0530

    env changed

--- client/utils/Firebase.js
+++ client/utils/Firebase.js
@@ -1,23 +1,45 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-import {getAuth, GoogleAuthProvider} from "firebase/auth"
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+// import {getAuth, GoogleAuthProvider} from "firebase/auth"
 
-// Your web app's Firebase configuration
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+//   authDomain: "loginvirtualcourses-db718.firebaseapp.com",
+//   projectId: "loginvirtualcourses-db718",
+//   storageBucket: "loginvirtualcourses-db718.firebasestorage.app",
+//   messagingSenderId: "808911078534",
+//   appId: "1:808911078534:web:0caba9e9d193e4a3e5bd07"
+// };
+
+
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+
+// const auth = getAuth(app)
+// const provider = new GoogleAuthProvider()
+
+// export {auth,provider}
+
+//-----------------------------------------------------------------------------
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
-  authDomain: "loginvirtualcourses-db718.firebaseapp.com",
-  projectId: "loginvirtualcourses-db718",
-  storageBucket: "loginvirtualcourses-db718.firebasestorage.app",
-  messagingSenderId: "808911078534",
-  appId: "1:808911078534:web:0caba9e9d193e4a3e5bd07"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-const auth = getAuth(app)
-const provider = new GoogleAuthProvider()
+export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider();
+export default app;
 
-export {auth,provider}
